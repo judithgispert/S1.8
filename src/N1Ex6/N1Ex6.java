55102fe3d03304f805898d3f7a7327e1dcca185e
judithgispert <judithgispert@10.63.0.51>
2023-10-18 17:04:24 +0200

    List length order

--- src/N1Ex6/N1Ex6.java
+++ src/N1Ex6/N1Ex6.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class N1Ex6 {
 	
 	public static void main (String[]args) {
 		
-		List<String> stringList = Arrays.asList("Joan", "22", "Flower", "Alex", "13", "84791");
+		List<String> stringList = Arrays.asList("Joan", "22", "Green flower", "Alex", "13", "8453791");
 	
-		stringList.stream().sorted(Comparator.comparing(String::length))
-				.forEach(System.out::println);
+		stringList.stream().sorted(Comparator.comparing(String::length)).forEach(System.out::println);
 
 	}
 
